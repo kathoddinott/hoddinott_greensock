ff7ebf7edff02841bf5e88d70f46535c2ac5fcf0
kathoddinott <43182997+kathoddinott@users.noreply.github.com>
2019-03-25 16:30:10 -0400

    Add files via upload

---
+++ hoddinott_greensock/js/main.js
@@ -0,0 +1,14 @@
+(() => {
+
+	let badge = document.querySelector('img');
+
+	function animImage() {
+		TweenMax.to(badge, 0.7, { scaleX: 1.025, scaleY: 1.025, rotation: 50, ease:Elastic. easeInOut, onComplete: animFooterText });
+	}
+
+	function animFooterText() {
+		TweenMax.to('h3', 0.7, { scaleX:2, scaleY: 2, ease:Elastic. easeInOut})
+	}
+	badge.addEventListener('mouseover', animImage);
+
+})();
